69bb49bb63d645db06f4edd3fd67b131e0e964a2
Michael Tran <revengex@gamerarmy.com>
2013-10-21 22:05:42 -0500

    Final Revision of Maze Before Presentations

---
+++ Assets/Levels/michael_level/CatchPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatchPlayer : MonoBehaviour {
+	
+	public AudioClip audioClip;
+	private float resetAfterDeathTime = 5f;
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			Destroy (other.gameObject);
+			AudioSource gg = this.gameObject.GetComponent<AudioSource>();
+			gg.Stop();
+			AudioSource.PlayClipAtPoint(audioClip, transform.position);
+			Invoke ("DoReset", resetAfterDeathTime);
+		}
+	}
+	
+	void DoReset()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+}
