1edfcc854e153233f5b768eb249487affc90a9af
Michael Tran <revengex@gamerarmy.com>
2013-11-14 16:08:47 -0600

    Added an intro type of camera (not fully finished yet)

---
+++ Assets/Levels/michael_level/CutsceneCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneCamera : MonoBehaviour {
+
+	void Start () {
+		Debug.Log ("Calling Destroy Camera");
+		Invoke ("DestroyCamera", 11f);
+	}
+	
+	void DestroyCamera(){
+		Debug.Log ("Destroy Camera");
+	}
+}
---
+++ Assets/Levels/michael_level/MazeEnd.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeEnd : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other){
+		
+	}
+	
+}
