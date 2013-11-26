60198e7515fb2876a327a984c78e02a3fc802936
Michael Tran <revengex@gamerarmy.com>
2013-11-25 21:33:40 -0600

    Updated maze level with an ending scene w/ a script for level transitions Updated to a more accurate/better pathing for the ghosts to use (removed smooth pathing as it was causing unwanted behavior) Added a journal entry containing clues to help the player through the maze

---
+++ Assets/Levels/michael_level/EndLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+	void Start () {
+		Invoke ("LevelTransition", 6f);
+	}
+	
+	void LevelTransition() {
+		Debug.Log ("Level transition script here.");
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
--- Assets/Levels/michael_level/MazeEnd.cs
+++ Assets/Levels/michael_level/MazeEnd.cs
@@ -2,9 +2,16 @@
 using System.Collections;
 
 public class MazeEnd : MonoBehaviour {
+	public GameObject endCamera;
+	private MazeEnd mazeEnd;
 	
 	void OnTriggerEnter(Collider other){
-		
+		if(other.CompareTag("Player"))
+		{
+			Instantiate (endCamera, endCamera.transform.position, endCamera.transform.rotation);
+			mazeEnd = (MazeEnd) GetComponent("MazeEnd");
+			mazeEnd.enabled = false;
+		}
 	}
 	
 }
