8a00169f5c795915c479bcb6d0e73e21ddfafbd0
mgraham2021 <m.gram2021@gmail.com>
2014-01-04 13:34:40 -0600

    started putting together my enemy stuff

---
+++ Assets/enemyCollider.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var playerSpawn : Transform; //drag your spawn point to here in the inspector - probably make it an empty gameobject so you can move it about on the diving board
+ 
+function OnTriggerEnter(other : Collider)
+{
+    if (other.tag == "Player")
+    {
+        other.transform.position = playerSpawn.position;
+    }
+}
---
+++ Assets/movingEnemy.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var timer : float = 0.0;
+var timerMax : float = 1.5;
+var randDir : int = 1;
+var Hspeed : int = 20;
+var person : GameObject;
+protected var startHeight   : float;
+
+function Start () {
+startHeight = transform.localPosition.y;
+}
+
+ 
+function Update()
+{
+    if ( timer > timerMax )
+    {
+        // pick a new direction
+        randDir = Random.Range(1,5);
+        // make a new random Max time
+        timerMax = Random.Range( 0.5, 2.5 );
+    }
+ 
+    // movement here
+    // ....
+    switch( randDir )
+    {
+        case 1 :
+            person.transform.Translate(-Vector2.up*Hspeed*Time.deltaTime);
+        break;
+        case 2 :
+            person.transform.Translate(Vector2.up*Hspeed*Time.deltaTime);
+        break;
+        case 3 :
+            person.transform.Translate(Vector2.right*Hspeed*Time.deltaTime);
+        break;
+        case 4 :
+            person.transform.Translate(-Vector2.right*Hspeed*Time.deltaTime);
+        break;
+    }
+}
