77c8373d75ef78c3683ed72d8b449242839c21ad
Michael Graham <m.gram2021@gmail.com>
2014-01-10 15:43:09 -0800

    Here is AI script and Boids script for the beach ghost.

---
+++ Assets/AI.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour 
+{
+
+
+	public bool usingAStar;
+	public float distanceUntilFade = 3;
+	public float fadeSpeed = 5;
+	public float turnSpeed = 10;
+	public float walkSpeed = 5;
+	public float runSpeed = 10;
+	//flocking algorithm
+	public Boids boids = new Boids ();
+
+	private float timer = 0;
+	private Vector3 randLoc = Vector3.zero;
+	private AIState state;
+	private float currentSpeed;
+	public float CurrentSpeed { get { return currentSpeed; } set { currentSpeed = value; } }
+
+	private const float DISTANCE_FROM_SELF_FOR_RANDOM = 4; 
+	private const float DISTANCE_UNTIL_GO_TO_IDLE = 1;
+
+	public bool Idle { get; set; }
+
+	void Start()
+	{
+		currentSpeed = runSpeed;
+	}
+
+
+
+
+	void Update()
+	{
+		switch (state)
+		{
+		case AIState.Idle:
+
+			timer -= Time.deltaTime;
+
+			if(!Idle)
+			{
+				timer = Random.Range(0f, 2f);
+				Idle = true;
+			}
+
+			if(timer <= 0)
+			{
+				state = usingAStar ? AIState.AStarWander : AIState.Wander;
+				Idle = false;
+			}
+			break;
+
+		case AIState.AStarWander:
+			break;
+
+		case AIState.Wander:
+			if(randLoc == Vector3.zero)
+				randLoc = new Vector3(Random.insideUnitCircle.x, transform.position.y, Random.insideUnitCircle.y) * DISTANCE_FROM_SELF_FOR_RANDOM;
+			
+			randLoc.y = transform.position.y;
+
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(randLoc - transform.position), Time.deltaTime * turnSpeed);
+
+			currentSpeed = Mathf.Lerp (currentSpeed, Vector3.Distance(randLoc, transform.position) <= distanceUntilFade ? walkSpeed : runSpeed, Time.deltaTime * fadeSpeed);
+
+			if(Vector3.Distance(transform.position, randLoc) <= DISTANCE_UNTIL_GO_TO_IDLE)
+			{
+				randLoc = Vector3.zero;
+				state = AIState.Idle;
+			}
+			break;
+
+		}
+
+		boids.Update (this);
+
+		//movement controller
+		if (!Idle)rigidbody.AddRelativeForce (Vector3.forward * currentSpeed);
+
+		if (rigidbody.velocity.magnitude > currentSpeed * .3f && currentSpeed > walkSpeed) 
+						rigidbody.velocity = transform.forward * currentSpeed;
+
+
+		//steering Controller  
+		if (Physics.Raycast (transform.position, transform.forward, 2)) {
+			{
+				var direction = transform.right;
+				if(Physics.Raycast(transform.position, (transform.forward + transform.right).normalized, 2))
+				{
+					direction = -direction;
+
+					if(Physics.Raycast(transform.position, (transform.forward - transform.right).normalized, 2))
+						direction = -transform.forward; 
+				}
+				transform.rotation = Quaternion.Slerp(Transform.rotation, Quaternion.LookRotation (direction), Time.deltaTime * turnSpeed);
+			}
+    }
+}
+
+public enum AIState
+{
+	Idle,
+	AStarWander,
+	Wander
+
+} ;
+
+
+
+	
---
+++ Assets/Boids.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+
+[System.Serializable]
+ public class Boids 
+{
+	public string tagToSearch;
+	public float searchRadius;
+
+	public AI leader;
+	public List<AI> boidGroup = new List<AI>();
+
+	public BoidState boidState = BoidState.Ignore;
+	public float seperationRadius;
+
+	public void Update(AI ai)
+	{
+		var goa = GameObject.FindGameObjectWithTag (tagToSearch);
+
+		foreach (var gameObject in goa.Where(gameObject => Vector3.Distance(gameObject.transform.position, ai.transform.position) <=searchRadius).Where(gameObject =>gameObject.GetComponent<AI>() && !boidGroup.Contains (gameObject.GetComponent<AI>()))) {
+						boidGroup.Add (gameObject.GetComponent<AI> ());
+				}
+
+		if (leader = null && boidGroup.Count > 0) {
+				
+			leader = boidGroup[Random.Range (0, boidGroup.Count)];
+			foreach (var boid in boidGroup)
+			{
+				if(boid.boids.leader != leader && boid.boids.leader != null) leader = boid.boids.leader;
+			}
+		}
+		if (leader == ai) 
+		{
+			boidState = BoidState.Ignore;
+			foreach (var boid in boidGroup.Where (boid => boid.boids.leader !=ai).Where (boid => boid.boids.leader == boid || boid.boids.leader == null))
+				boid.boids.leader = ai;
+			return;
+		}
+
+		foreach (var boid in boidGroup)
+						boid.boids.leader = leader;
+		if (leader != null) {
+			//ai.aStar = null; 
+			if(boidState == BoidState.Ignore)
+				boidState = 0;
+		}
+		//else ai.astar = null;
+
+		ai.Idle = leader.Idle;
+		switch (BoidState) {
+				
+			case BoidState.Alignment:
+			ai.transform.rotation = Quaternion.Slerp(ai.transform.rotation, leader.transform.rotation, Time.deltaTime * ai.turnSpeed);
+			ai.CurrentSpeed = leader.CurrentSpeed;
+
+			if(Vector3.Distance(ai.transform.position, leader.transform.position) <= seperationRadius / 2)
+			boidState = BoidState.Seperation;
+
+			if(Vector3.Distance(ai.transform.position, leader.transform.position) >= seperationRadius)
+				boidState = BoidState.Cohesion;
+
+
+				
+			break;
+
+
+			case BoidState.Cohesion:
+			ai.CurrentSpeed = ai.runSpeed;
+			ai.transforn.rotation = Quaternion.Slerp(ai.transform.rotation, Quaternion.LookRotation(leader.transform.position - leader.transform.forward * -1) - ai.transform.position), Time.deltaTime * ai.turnSpeed);
+
+			if(Vector3.Distance(ai.transform.position, leader.transform.position) <= seperationRadius - .5f)
+				boidState = 0;
+			break;
+
+			case BoidState.Seperation:
+			ai.CurrentSpeed = ai.runSpeed;
+			ai.transforn.rotation = Quaternion.Slerp(ai.transform.rotation, Quaternion.LookRotation(ai.transform.position - leader.transform.position), Time.deltaTime * ai.turnSpeed);
+
+
+		    if(Vector3.Distance(ai.transform.position, leader.transform.position) >= seperationRadius /2)
+				boidstate = 0;
+			break;
+
+			case BoidState.Ignore:
+			break;
+			}
+
+		foreach (var boid in boidGroup)
+		{
+			if(Vector3.Distance (boid.transform.position, ai.transform.position) <= seperationRadius / 2)
+				ai.rigidbody.AddForce(ai.transform.position - boid.transform.position).normalized * ai.runSpeed * Time.deltaTime);
+		}
+	}
+
+}
+
+public enum BoidState
+{
+	Alignment = 0,
+	Cohesion = 1,
+	Seperation = 2,
+	Ignore = -1
+} ;
