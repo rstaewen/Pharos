aa236394c9290cce3ffe28f506133ba9bdeabfc1
mgraham2021 <m.gram2021@gmail.com>
2014-01-21 15:51:17 -0600

    Updates to mirrorRoom and cleaning up on Air Level

---
+++ Assets/sceneTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneTrigger : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		Application.LoadLevel("mirrorRoom");
+
+
+	}
+}
