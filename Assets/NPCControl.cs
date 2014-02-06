7cedbbb7baf534da4cd80e9230e695739fd7f417
mgraham2021 <m.gram2021@gmail.com>
2014-02-06 15:36:58 -0600

    RAIN

--- Assets/NPCControl.cs
+++ Assets/NPCControl.cs
@@ -1,132 +1,15 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using UnityEngine;
-//
-//[RequireComponent(typeof(Rigidbody))]
-//public class NPCControl : MonoBehaviour
-//{
-//	public GameObject player;
-//	public Transform[] path;
-//	private FSMSystem fsm;
-//	
-//	public void SetTransition(Transition t) { fsm.PerformTransition(t); }
-//	
-//	public void Start()
-//	{
-//		MakeFSM();
-//	}
-//	
-//	public void FixedUpdate()
-//	{
-//		fsm.CurrentState.Reason(player, gameObject);
-//		fsm.CurrentState.Act(player, gameObject);
-//	}
-//	
-//	// The NPC has two states: FollowPath and ChasePlayer
-//	// If it's on the first state and SawPlayer transition is fired, it changes to ChasePlayer
-//	// If it's on ChasePlayerState and LostPlayer transition is fired, it returns to FollowPath
-//	private void MakeFSM()
-//	{
-//		FollowPathState follow = new FollowPathState(path);
-//		follow.AddTransition(Transition.SawPlayer, StateID.ChasingPlayer);
-//		
-//		ChasePlayerState chase = new ChasePlayerState();
-//		chase.AddTransition(Transition.LostPlayer, StateID.FollowingPath);
-//		
-//		fsm = new FSMSystem();
-//		fsm.AddState(follow);
-//		fsm.AddState(chase);
-//	}
-//}
-//
-//public class FollowPathState : FSMState
-//{
-//	private int currentWayPoint;
-//	private Transform[] waypoints;
-//	
-//	public FollowPathState(Transform[] wp) 
-//	{ 
-//		waypoints = wp;
-//		currentWayPoint = 0;
-//		stateID = StateID.FollowingPath;
-//	}
-//	
-//	public override void Reason(GameObject player, GameObject npc)
-//	{
-//		// If the Player passes less than 15 meters away in front of the NPC
-//		RaycastHit hit;
-//		if (Physics.Raycast(npc.transform.position, npc.transform.forward, out hit, 15F))
-//		{
-//			if (hit.transform.gameObject.tag == "Player")
-//				npc.GetComponent<NPCControl>().SetTransition(Transition.SawPlayer);
-//		}
-//	}
-//	
-//	public override void Act(GameObject player, GameObject npc)
-//	{
-//		// Follow the path of waypoints
-//		// Find the direction of the current way point 
-//		Vector3 vel = npc.rigidbody.velocity;
-//		Vector3 moveDir = waypoints[currentWayPoint].position - npc.transform.position;
-//		
-//		if (moveDir.magnitude < 1)
-//		{
-//			currentWayPoint++;
-//			if (currentWayPoint >= waypoints.Length)
-//			{
-//				currentWayPoint = 0;
-//			}
-//		}
-//		else
-//		{
-//			vel = moveDir.normalized * 10;
-//			
-//			// Rotate towards the waypoint
-//			npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
-//			                                          Quaternion.LookRotation(moveDir),
-//			                                          5 * Time.deltaTime);
-//			npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
-//			
-//		}
-//		
-//		// Apply the Velocity
-//		npc.rigidbody.velocity = vel;
-//	}
-//	
-//} // FollowPathState
-//
-//public class ChasePlayerState : FSMState
-//{
-//	public ChasePlayerState()
-//	{
-//		stateID = StateID.ChasingPlayer;
-//	}
-//	
-//	public override void Reason(GameObject player, GameObject npc)
-//	{
-//		// If the player has gone 30 meters away from the NPC, fire LostPlayer transition
-//		if (Vector3.Distance(npc.transform.position, player.transform.position) >= 30)
-//			npc.GetComponent<NPCControl>().SetTransition(Transition.LostPlayer);
-//	}
-//	
-//	public override void Act(GameObject player, GameObject npc)
-//	{
-//		// Follow the path of waypoints
-//		// Find the direction of the player 		
-//		Vector3 vel = npc.rigidbody.velocity;
-//		Vector3 moveDir = player.transform.position - npc.transform.position;
-//		
-//		// Rotate towards the waypoint
-//		npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
-//		                                          Quaternion.LookRotation(moveDir),
-//		                                          5 * Time.deltaTime);
-//		npc.transform.eulerAngles = new Vector3(0, npc.transform.eulerAngles.y, 0);
-//		
-//		vel = moveDir.normalized * 10;
-//		
-//		// Apply the new Velocity
-//		npc.rigidbody.velocity = vel;
-//	}
-//	
-//} // ChasePlayerState
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCcontrol : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
