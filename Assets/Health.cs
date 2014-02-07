ff0e23df8611ef8992a80494550411da945b5525
Michael Graham <m.gram2021@gmail.com>
2014-02-07 15:53:22 -0600

    work so far on rigging ghost character

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using RAIN.Core;
+
+	public class Health : MonoBehaviour 
+{
+
+	public float health = 100f;
+	private AIRig aiRig = null;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		aiRig = gameObject.GetComponentInChildren<AIRig> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		aiRig.AI.WorkingMemory.SetItem ("health", health);
+	}
+}
