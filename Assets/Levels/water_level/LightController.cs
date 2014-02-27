d0653ec59f69d814d1d2de8a6406b25b14ebff83
Jingjing Yu <jingzephyr@gmail.com>
2014-02-27 16:08:11 -0600

    change by 2-27-2014

---
+++ Assets/Levels/water_level/LightController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+	public GameObject[] Lights; 
+	private bool[] CheckArray;
+	private float starttimer;
+	private float currenttime;
+	private float allowedtime = 120f;
+	private Color startcolor;
+	private bool started = false;
+	void Start () {
+		starttimer = Time.time;
+		Debug.Log (starttimer);
+		CheckArray = new bool[Lights.Length];
+		InitializeCheckArray ();
+		Light mylight;
+		mylight = Lights[0].GetComponent<Light> ();
+		startcolor = mylight.color;
+	}
+
+	void Update () {
+		currenttime = Time.time;
+		if (((currenttime - starttimer) > allowedtime)&& started == true) {
+			ResetLights();	
+
+		}
+
+		
+	}
+	void ResetLights (){
+		InitializeCheckArray ();
+		for (int i =0; i < Lights.Length; i++) {
+			Light mylight;
+			mylight = Lights[i].GetComponent<Light> ();	
+			mylight.color = startcolor;
+		}
+		started = false;
+
+	}
+	void InitializeCheckArray(){
+		for (int i=0; i<CheckArray.Length; i++) {
+			CheckArray[i]=false;
+		}
+	}
+	void Change_Color(GameObject light){
+		Light mylight;
+		mylight = light.GetComponent<Light> ();
+		//mylight.enabled = !mylight.enabled;
+		mylight.color = Color.white;
+
+	}
+	public bool Check ( int index){
+		Debug.Log ("Check Entered");
+		if (index == 0) {
+			starttimer = Time.time;		
+			started = true;
+		}
+		if (index == 0 || CheckArray [index - 1] == true) {
+			Change_Color(Lights[index]);
+			CheckArray[index] = true;
+			return true;
+		}
+
+		return false;
+	}
+}
---
+++ Assets/Levels/water_level/LightDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightDetector : MonoBehaviour {
+	public int lightnumber;
+	public Transform targetlocation;
+	public float speed = 5f;
+	private LightController lightController;
+	private bool move = true;
+
+	void Start(){
+		lightController = GameObject.FindGameObjectWithTag ("LightController").GetComponent<LightController>();
+
+	}
+	void OnTriggerEnter(Collider other){
+		lightController.Check (lightnumber-1);
+
+	}
+	void Update(){
+		if (move == true && targetlocation != null) {
+			moveLight();		
+		}
+
+	}
+	void moveLight(){
+		Vector3 targetdirection = targetlocation.position - transform.position;
+		Vector3 targetarea = new Vector3(targetlocation.position.x, targetlocation.position.y,targetlocation.position.z);
+		transform.position = Vector3.MoveTowards (transform.position, targetarea, speed*Time.deltaTime);
+
+	}
+}
